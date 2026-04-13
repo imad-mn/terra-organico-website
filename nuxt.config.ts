209d042e722412e2bba0bafc3b148f4b8ac9acb2
Imad Marmoud <imadmarmoud@gmail.com>
2026-04-13 12:20:06 -0600

    Tratar de arreglar node

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -39,4 +39,12 @@ export default defineNuxtConfig({
       ],
     },
   },
+  vite: {
+    optimizeDeps: {
+      include: [
+        '@vue/devtools-core',
+        '@vue/devtools-kit',
+      ]
+    }
+  }
 })
